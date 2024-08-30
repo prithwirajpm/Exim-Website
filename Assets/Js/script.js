0eff7157be4ca54f3e41c71747f17aec6eaa76f3
prithwirajpm <wt.prithwiraj@gmail.com>
2024-08-30 20:06:22 +0530

    change button

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -19,18 +19,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function toggleMenu() {
     const classTag = document.querySelector('.nav-links-main');
+    
     if (classTag.style.display === 'none' || classTag.style.display === '') {
       classTag.style.display = 'block'; 
-      classTag.classList.toggle('active');// Show the menu
+      classTag.classList.add('active'); // Show the menu
     } else {
       classTag.style.display = 'none'; // Hide the menu
+      classTag.classList.remove('active');
     }
+   
   }
+  
 
 
   function maplocation(){
     window.open("https://www.google.com/maps/place/Al+Fattan+Plaza/@25.2542159,55.3338822,13z/data=!3m1!5s0x3e5f69c8fc76ce65:0x94e7dcb4d547dd25!4m6!3m5!1s0x3e5f5d1b81d61725:0xc4c3354de3a5d9eb!8m2!3d25.2512168!4d55.3450721!16s%2Fg%2F11bbwxlx9m?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D");
   }
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const video = document.getElementById('promo-video');
+    const section = document.querySelector('.find-us');
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    observer.observe(section);
+  });
   
   
 
