2415f0815b30d8f150059679e1e470deda99a8d7
prithwirajpm <wt.prithwiraj@gmail.com>
2024-09-09 21:28:14 +0530

    set resource menu mobile view

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -39,6 +39,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
 
+  function toggleResource() {
+    const tag = document.querySelector('.list-resource');
+    if (tag.style.display === 'none' || tag.style.display === '') {
+      tag.style.display = 'block';
+    } else {
+      tag.style.display = 'none';
+    }
+  }
 
   function maplocation(){
     window.open("https://www.google.com/maps/place/Al+Fattan+Plaza/@25.2542159,55.3338822,13z/data=!3m1!5s0x3e5f69c8fc76ce65:0x94e7dcb4d547dd25!4m6!3m5!1s0x3e5f5d1b81d61725:0xc4c3354de3a5d9eb!8m2!3d25.2512168!4d55.3450721!16s%2Fg%2F11bbwxlx9m?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D");
